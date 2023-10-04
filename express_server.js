2845b71c34b2d69eebf5f74202c39eea1e633bff
KenniCodes <kennedygosnell@outlook.com>
2023-10-04 15:13:00 -0700

    fixed error in package script where npm start was not functioning as intended, fixed _header to properly display login/logout form based on if user was logged in/out, added server response to clearCookie when receiving /logout request

--- express_server.js
+++ express_server.js
@@ -30,8 +30,8 @@ app.get("/", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { 
-    urls: urlDatabase,
-    username: req.cookies["username"]
+    username: req.cookies["username"],
+    urls: urlDatabase
    };
   res.render("urls_index", templateVars);
 });
@@ -50,7 +50,9 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = {
+    username: req.cookies["username"]
+  };
   res.render("urls_new", templateVars);
 });
 
@@ -58,7 +60,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"] 
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -78,6 +80,11 @@ app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
   console.log("username =", req.body.username);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.get("/urls.json", (req, res) => {
